b8c491634c719e79c6a3162d8afcbe5db4887a45
upeksha434 <upeksha434@gmail.com>
2023-09-07 21:26:10 +0530

    schema moded

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,9 +7,51 @@ import { jwt_config } from 'src/config/jwt';
 import { LoginDto } from './dto/login.dto';
 import { User } from '@prisma/client';
 
+import * as Brevo from '@getbrevo/brevo';
+
 @Injectable()
 export class AuthService {
+
+
+
+
   constructor(private prisma: PrismaService, private jwtService: JwtService) { }
+
+  async sendOTPEmail(otp: string, email: string, name: string) {
+    const defaultClient = Brevo.ApiClient.instance;
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = "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 "
+
+    const apiInstance = new Brevo.TransactionalEmailsApi();
+    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+
+    sendSmtpEmail.subject = `Common: Your OTP CODE Is ${otp}`;
+    sendSmtpEmail.htmlContent = `
+      <html>
+        <head>
+          <title>Common: Your OTP CODE Is ${otp}</title>
+        </head>
+        <body>
+          <h1>Your OTP CODE Is ${otp}</h1>
+          <p>Hi ${name},</p>
+          <p>Your OTP CODE Is ${otp}</p>
+          <p>Thanks</p>
+        </body>
+      </html>
+    `;
+    sendSmtpEmail.sender = { "name": "SurvayY Platform", "email": "no-reply@survwayy.com" };
+    sendSmtpEmail.to = [
+      { "email": email, "name": name }
+    ];
+
+    sendSmtpEmail.replyTo = { "email": "example@brevo.com", "name": "sample-name" };
+
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data:any) {
+      console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+    }, function (error) {
+      console.error(error);
+    });
+  }
 
   async register(data: RegisterDto) {
     const checkUserExists = await this.prisma.user.findFirst({
@@ -18,6 +60,7 @@ export class AuthService {
       },
     });
 
+
     if (checkUserExists) {
       throw new HttpException('User already registered', HttpStatus.FOUND);
     }
@@ -25,13 +68,72 @@ export class AuthService {
     const createUser = await this.prisma.user.create({
       data: data,
     });
+
+    // if(checkUserExists.roleId == 1){
+    //   const org = await this.prisma.organization.create({
+    //     data:{
+    //       orgname: "OrgName", /// Get the name from the DTO
+    //       adminId: createUser.id
+    //     }
+    //   });
+    // }
+
     if (createUser) {
+
+      const otp = Math.floor(100000 + Math.random() * 900000);
+
+      // Send Email
+      this.sendOTPEmail(otp.toString(), createUser.email, createUser.fname + ' ' + createUser.lname);
+
+      // Create Email Verification Record
+      await this.prisma.emailVerifications.create({
+        data: {
+          otp: otp.toString(),
+          userId: createUser.id
+        }
+      });
+
       return {
-        statusCode: 200,
-        message: 'Register Successfull',
+        message: 'Sign Up Successfull!',
       };
     }
   }
+
+
+
+  async emailVerification(otp: string, email: string) {
+    const checkOtp = await this.prisma.emailVerifications.findFirst({
+      where: {
+        otp: otp,
+        user: {
+          email: email
+        },
+        createdAt: {
+          gte: new Date(new Date().getTime() - 5 * 60 * 1000)
+        }
+      }
+    });
+
+    if (!checkOtp) {
+      throw new HttpException('Invalid OTP', HttpStatus.UNAUTHORIZED);
+    } else {
+      const updateUser = await this.prisma.user.update({
+        where: {
+          id: checkOtp.userId
+        },
+        data: {
+          isVerified: true
+        }
+      });
+
+      if (updateUser) {
+        return {
+          message: 'Email Verified Successfully!'
+        }
+      }
+    }
+  }
+
   //*************************************************************************** */
   //for login
 
@@ -69,23 +171,6 @@ export class AuthService {
       );
     }
   }
-
-  async getUser(email: string): Promise<User> {
-
-
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email
-      }
-    });
-
-    if (!user) {
-      throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
-    }
-
-    return user;
-  }
-
 
 
   generateJWT(payload: any) {
